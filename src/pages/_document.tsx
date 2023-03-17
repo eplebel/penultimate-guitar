feba7fbce70e30580a6e92e6f7f67a36d0209fa3
Etienne P. LeBel <etienne.lebel@gmail.com>
2023-03-17 16:00:28 -0600

    added outer wrapper for nice app border

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,8 +5,10 @@ export default function Document() {
     <Html lang="en">
       <Head></Head>
       <body className="px-4">
-        <Main />
-        <NextScript />
+        <div className="max-w-lg mx-auto my-2 px-4 rounded-lg border-[1px] border-gray" >
+			<Main />
+        	<NextScript />
+		</div>
       </body>
     </Html>
   );
